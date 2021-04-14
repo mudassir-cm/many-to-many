e4e9052bc3d370a0528d05797078ff789f1ffdcd
mudassir-cm <mudasir2021@gmail.com>
2021-04-14 12:59:49 +0500

    m2m crud is complete

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('cast/add', views.castadd),
     path('cast/show', views.castshow),
     path('movie/show', views.movieshow),
+    path('cast/edit/<int:id>', views.castedit),
+    path('cast/update/<int:id>', views.castupdate),
+    path('cast/delete/<int:id>', views.castdelete),
+    path('movie/delete/<int:id>', views.moviedelete),
+    path('movie/edit/<int:id>', views.movieedit),
+    path('movie/update/<int:id>', views.movieupdate),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -28,9 +28,40 @@ def castadd(request):
 def castshow(request):
     list = Cast.objects.all()
     return render(request, 'myapp/castshow.html', {'list': list})
+def castedit(request, id):
+    cast = Cast.objects.get(id=id)
+    return render(request, 'myapp/castedit.html', {'cast': cast})
+def castupdate(request, id):
+    cast = Cast.objects.get(id=id)
+    cast.name = request.POST['name']
+    cast.age = request.POST['age']
+   # castform = CastForm(request.POST, instance=cast)
+    cast.save()
+    return redirect('/myapp/cast/show')
+
+def castdelete(request, id):
+    cast = Cast.objects.get(id=id)
+    cast.delete()
+    return redirect('/myapp/cast/show')
 
 def movieshow(request):
     list = Movie.objects.all()
     return render(request, 'myapp/movieshow.html', {'list': list})
 
+def moviedelete(request, id):
+    movie = Movie.objects.get(id=id)
+    movie.delete()
+    return redirect('/myapp/movie/show')
+
+def movieedit(request, id):
+    movie = Movie.objects.get(id=id)
+    return render(request, 'myapp/movieedit.html', {'movie': movie})
+
+def movieupdate(request, id):
+    movie =  Movie.objects.get(id=id)
+    movie.name = request.POST['name']
+    movie.dor = request.POST['dor']
+   # movieform = MovieForm(request.POST, instance=movie)
+    movie.save()
+    return redirect('/myapp/movie/show')
 # Create your views here.
